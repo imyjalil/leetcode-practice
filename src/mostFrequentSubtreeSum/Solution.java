75e62fa92257d21449b500ddb564b93e9721f59a
Younus Jalil <31122173+imyjalil@users.noreply.github.com>
2021-12-14 14:17:08 +0530

    508. Most Frequent Subtree Sum

---
+++ src/mostFrequentSubtreeSum/Solution.java
@@ -0,0 +1,26 @@
+package mostFrequentSubtreeSum;
+
+public class Solution {
+	Map<Integer,Integer>map;int maxCount=Integer.MIN_VALUE;
+    public int[] findFrequentTreeSum(TreeNode root) {
+        map=new HashMap<>();
+        solve(root);
+        List<Integer>res=new ArrayList<>();
+        for(Map.Entry<Integer,Integer>s:map.entrySet())
+            if(s.getValue()==maxCount)
+                res.add(s.getKey());
+        int[] arr = new int[res.size()];
+        for(int i=0;i<arr.length;i++) arr[i]=res.get(i);
+        return arr;
+    }
+    public int solve(TreeNode root)
+    {
+        if(root == null) return 0;
+        int left=solve(root.left);
+        int right=solve(root.right);
+        int total=root.val+left+right;
+        map.put(total,map.getOrDefault(total,0)+1);
+        maxCount=Math.max(maxCount,map.get(total));
+        return total;
+    }
+}
