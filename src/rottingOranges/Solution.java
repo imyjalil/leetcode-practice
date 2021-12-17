6f7431aaa3166bf4a262781060a8cd46d551d36e
Younus Jalil <31122173+imyjalil@users.noreply.github.com>
2021-12-17 11:49:43 +0530

    994. Rotting Oranges

---
+++ src/rottingOranges/Solution.java
@@ -0,0 +1,42 @@
+package rottingOranges;
+
+//problem link: https://leetcode.com/problems/rotting-oranges/
+//Author: imyjalil
+public class Solution {
+	public int orangesRotting(int[][] grid) {
+        int cnt=0, n=grid.length, m=grid[0].length;
+        Queue<int[]>q=new LinkedList<>();
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(grid[i][j]==1)
+                    cnt++;
+                else if(grid[i][j]==2)
+                    q.offer(new int[]{i,j});
+            }
+        }
+        int steps=0;
+        if(cnt==0) return steps;
+        int dirs[]=new int[]{1,0,-1,0,1};
+        while(!q.isEmpty())
+        {
+            ++steps;
+            int len=q.size();
+            for(int i=0;i<len;i++)
+            {
+                int loc[]=q.poll();
+                for(int j=0;j<4;j++)
+                {
+                    int nr=loc[0]+dirs[j], nc=loc[1]+dirs[j+1];
+                    if(nr<0 || nc<0 || nr>=n || nc>=m || grid[nr][nc]!=1) continue;
+                    cnt--;
+                    if(cnt==0) return steps;
+                    grid[nr][nc]=2;
+                    q.offer(new int[]{nr, nc});
+                }
+            }
+        }
+        return -1;
+    }
+}
