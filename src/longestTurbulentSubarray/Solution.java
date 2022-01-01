bb5d62c2652566e67ad742a52744a73c6a806f88
Younus Jalil <31122173+imyjalil@users.noreply.github.com>
2022-01-01 19:13:08 +0530

    978. Longest Turbulent Subarray

---
+++ src/longestTurbulentSubarray/Solution.java
@@ -0,0 +1,27 @@
+package longestTurbulentSubarray;
+
+public class Solution {
+	public int maxTurbulenceSize(int[] arr) {
+        int inc=1, dec=1, n = arr.length, ans=1;
+        for(int i=1;i<n;i++)
+        {
+            if(arr[i]>arr[i-1])
+            {
+                inc=dec+1;
+                dec=1;
+            }
+            else if(arr[i]<arr[i-1])
+            {
+                dec=inc+1;
+                inc=1;
+            }
+            else
+            {
+                inc=1;
+                dec=1;
+            }
+            ans = Math.max(ans, Math.max(inc, dec));
+        }
+        return ans;
+    }
+}
