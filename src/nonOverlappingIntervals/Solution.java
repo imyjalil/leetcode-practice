a12e961b243a7044e2ac51bcfeddc0d407669da0
Younus Jalil <31122173+imyjalil@users.noreply.github.com>
2022-01-07 12:12:18 +0530

    435. Non-overlapping Intervals

---
+++ src/nonOverlappingIntervals/Solution.java
@@ -0,0 +1,19 @@
+package nonOverlappingIntervals;
+
+public class Solution {
+	//TC:O(n*logn)
+    public int eraseOverlapIntervals(int[][] intervals) {
+        Arrays.sort(intervals, (a,b)->(a[1]-b[1]));
+        int cnt=1;
+        int end=intervals[0][1];
+        for(int i=0;i<intervals.length;i++)
+        {
+            if(intervals[i][0]>=end)
+            {
+                end=intervals[i][1];
+                cnt++;
+            }
+        }
+        return intervals.length-cnt;//total-overlapping=non-overlapping
+    }
+}
